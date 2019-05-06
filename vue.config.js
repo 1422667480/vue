3e267e21cfde5466f3f11af470bd696b9cbcd6c1
dengfujian <fujian.deng@shenjiapingtai.com>
2019-05-06 11:00:54 +0800

    路径别名配置

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,24 @@
+const path = require('path');
 module.exports = {
+    /* 别名配置 */
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@views', resolve('src/views'))
+            .set('@router', resolve('src/router'))
+            .set('@', resolve('src'))
+    },
     publicPath: './',
     productionSourceMap: false,
     pages: {
         index: {
             entry: 'src/pages/index/main.js',
             template: "src/pages/index/index.html",
-            filename: "index.html",  
+            filename: "index.html",
         },
         home: {
             entry: 'src/pages/home/main.js',
             template: "src/pages/home/home.html",
-            filename: "home.html",   
+            filename: "home.html",
         }
     },
     devServer: {
@@ -18,5 +26,11 @@ module.exports = {
         port: 8088,
         https: false,
     },
-
+}
+/**
+ * @description 配置绝对路径
+ * @param {*} dir
+ */
+function resolve (dir) {
+    return path.join(__dirname, dir);
 }
